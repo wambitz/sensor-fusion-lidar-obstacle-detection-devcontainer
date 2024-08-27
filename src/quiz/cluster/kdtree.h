989c8aa2bd7101e8099f9eb0d0aeed654cac6c50
juliocastillo <wambee@hotmail.com>
2024-08-26 17:37:08 -0700

    ci: run clang-format on clustering src

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -1,58 +1,55 @@
 /* \author Aaron Brown */
 // Quiz on implementing kd tree
 
-#include "../../render/render.h"
-
+#include "render.h"
 
 // Structure to represent node of kd tree
 struct Node
 {
-	std::vector<float> point;
-	int id;
-	Node* left;
-	Node* right;
+    std::vector<float> point;
+    int id;
+    Node *left;
+    Node *right;
 
-	Node(std::vector<float> arr, int setId)
-	:	point(arr), id(setId), left(NULL), right(NULL)
-	{}
+    Node(std::vector<float> arr, int setId)
+        : point(arr)
+        , id(setId)
+        , left(NULL)
+        , right(NULL)
+    {
+    }
 
-	~Node()
-	{
-		delete left;
-		delete right;
-	}
+    ~Node()
+    {
+        delete left;
+        delete right;
+    }
 };
 
 struct KdTree
 {
-	Node* root;
+    Node *root;
 
-	KdTree()
-	: root(NULL)
-	{}
+    KdTree()
+        : root(NULL)
+    {
+    }
 
-	~KdTree()
-	{
-		delete root;
-	}
+    ~KdTree()
+    {
+        delete root;
+    }
 
-	void insert(std::vector<float> point, int id)
-	{
-		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+    void insert(std::vector<float> point, int id)
+    {
+        // TODO: Fill in this function to insert a new point into the tree
+        // the function should create a new node and place correctly with in the root
+    }
 
-	}
-
-	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
-	{
-		std::vector<int> ids;
-		return ids;
-	}
-	
-
+    // return a list of point ids in the tree that are within distance of target
+    std::vector<int> search(std::vector<float> target, float distanceTol)
+    {
+        std::vector<int> ids;
+        return ids;
+    }
 };
-
-
-
-
